fb5a5015410093f5b89ccecd367e24c890fc584a
Travis Kraemer <travis.kraemer@gmail.com>
2016-11-01 15:26:06 -0500

    Added solution to FlowShop
    Hopefully is right, tried with sample input and it works. Haven't yet
    tested with large data sets.

---
+++ Flow Shop/FlowShop.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numSwathers,numStages;
+    int testCase = 1;
+    cin >> numSwathers >> numStages;
+    while(numSwathers != 0 || numStages != 0)
+    {
+        //Dynamically allocated array, using syntax for [x][y] is more difficult
+        //Manually do offsets
+        int* timeArray = new int[numSwathers*numStages];
+        //Read in all the data
+        for(int i = 0; i < numSwathers; i++)
+            for(int j = 0; j < numStages; j++)
+                cin >> timeArray[i*numStages + j];
+        //Process the first row
+        for(int i = 1; i < numStages; i++)
+            timeArray[i] += timeArray[i-1];
+        //Process all other rows
+        for(int i = 1; i < numSwathers; i++)
+        {
+            //Calculate when first stage finishes
+            timeArray[i*numStages] += timeArray[(i-1)*numStages];
+            //Calculate the rest of the stages
+            for(int j = 1; j < numStages; j++)
+            {
+                int prevSwather = timeArray[(i-1)*numStages + j];
+                int prevStage = timeArray[i*numStages + j-1];
+                //Add max of previous swather completign stage or previous stage of same swather
+                if(prevSwather > prevStage)
+                    timeArray[i*numStages + j] += prevSwather;
+                else
+                    timeArray[i*numStages + j] += prevStage;
+            }
+        }
+        //Do the Output
+        cout << "Case " << testCase++ << ":";
+        for(int i = 0; i < numSwathers; i++)
+        {
+            //Print time it finished (Print space before so the last one doesn't have space after it
+            cout << " " << timeArray[i*numStages + numStages - 1];
+        }
+        cout << endl;
+        //Free memory, else memory leak
+        delete[] timeArray;
+        cin >> numSwathers >> numStages;
+    }
+    return 0;
+}
